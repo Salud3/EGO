0ab8b10a6337c4ec1316728c0060c3d512decb84
Davo1908 <88945953+Davo1908@users.noreply.github.com>
2024-01-24 23:44:08 -0600

    1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Rigidbody2D rb;
+
+    public float velocityMove;
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+
+        ProcesingMove();
+    }
+
+    void ProcesingMove()
+    {
+        //Logica de Movimiento
+        float inputMovement = Input.GetAxis("Horizontal");
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(inputMovement * velocityMove, rb.velocity.y);
+    }
+}
