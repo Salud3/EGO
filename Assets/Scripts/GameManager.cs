1f7541c084f9b5a775feff04d46f8f62076b90e8
Davo1908 <88945953+Davo1908@users.noreply.github.com>
2024-01-26 17:47:54 -0600

    new

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,18 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public int TotalPoints { get { return totalPoints; } }
     public int totalPoints;
+    public GameObject panel;
 
     public void ScorePoints(int pointsToAdd)
     {
         totalPoints += pointsToAdd;
         Debug.Log(totalPoints);
+
+        if (totalPoints >= 100)
+        {
+            Time.timeScale = 0f;
+            ActivatePanel();
+        }
     }
 
     public void SubtractPoints(int pointsToSubtract)
@@ -25,10 +32,21 @@ public class GameManager : MonoBehaviour
         Instance = this;
     }
 
+    void ActivatePanel()
+    {
+        if(panel != null)
+        {
+            panel.SetActive(true);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (panel != null)
+        {
+            panel.SetActive(false);
+        }
     }
 
     // Update is called once per frame
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject[] elements;
-    [SerializeField] float secondSpawn = 0.5f;
+    [SerializeField] float secondSpawn = 0.3f;
     [SerializeField] float minTras;
     [SerializeField] float maxTras;
 
@@ -22,7 +22,16 @@ public class Spawner : MonoBehaviour
             var position = new Vector2(wanted, transform.position.y);
             GameObject gameObject = Instantiate(elements[Random.Range(0, elements.Length)], position, Quaternion.identity);
             yield return new WaitForSeconds(secondSpawn);
+            Destroy(gameObject, 5f);
         }
     }
-            
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (CompareTag("Floor"))
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //}
+
 }
