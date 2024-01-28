adc785a91882c204f89c40719b3d5aff99be9524
Davo1908 <88945953+Davo1908@users.noreply.github.com>
2024-01-27 22:16:05 -0600

    Credits

---
+++ Assets/Scripts/PanelsActivate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelsActivate : MonoBehaviour
+{
+    [SerializeField] public GameObject panelCredits;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
